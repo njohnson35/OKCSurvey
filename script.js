0fa5f6329080fcc4c92762401ec60a721826783f
njohnson35 <112568197+njohnson35@users.noreply.github.com>
2022-11-19 16:52:09 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var fan = prompt("Are you still mad at Kevin Durant");
+
+if (fan === "no") {
+    alert("Thank you for moving on");
+} else {
+    alert("Its been damn near 10 years, move on");
+}
+
